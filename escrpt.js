a7bf0483cd1e547b252c584d0d21dcf1ebec4a2f
piero <pierito2000huamani@gmail.com>
2022-12-03 21:39:03 -0500

    agrege la funcion para cambiar imagen de portada

--- escrpt.js
+++ escrpt.js
@@ -12,3 +12,26 @@ function mostrarMenu(){
     }
     
 }
+
+
+function cambiarImagenPortada(number,punto){
+    let imagen=document.getElementById("imagen-portada");
+    let puntos=document.getElementsByClassName("punto");
+    console.log(puntos.length);
+    for(let i=0;i<puntos.length;i++){
+        puntos[i].style.background="white";
+        console.log(puntos[i]);
+    }
+
+    if(number==1){
+        imagen.setAttribute("src","imagenes/portada1-cl.png");
+        punto.style.background="black";
+    }else if(number==2){
+        
+        imagen.setAttribute("src","imagenes/portada2-cl.jpg");
+        punto.style.background="black";
+    }else{
+        
+        punto.style.background="black";
+    }
+}
